0ac14dbe4a9145bd519be3ddf11f28b2c99c0090
maharajan1502 <129009154+maharajan1502@users.noreply.github.com>
2024-08-21 03:05:31 -0700

    Update Breadcrum.jsx

--- src/Components/breadcums/Breadcrum.jsx
+++ src/Components/breadcums/Breadcrum.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Breadcrum.css'
-import rar from '../Assets/rarr.svg'
+import rar from '../Assets/sstar.png'
  const Breadcrum = (props) => {
     const { Product } = props;
    return (
